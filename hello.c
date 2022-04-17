c5c2ffe0825dfdc92dbaa7972288627b594dc506
Melancholy_YQ <1500861686@qq.com>
2022-04-17 16:25:31 +0800

    add some text in master

--- hello.c
+++ hello.c
@@ -4,9 +4,19 @@
 #include "stdio.h"
 #include "string.h"
 
+extern  void test(void);
+
 int main(void)
 {
     printf("hello world!\n");
 
     return  0;
+}
+/**
+ * @brief   add test in master.
+ * 
+ */
+void test(void)
+{
+    printf("master add v0.1\n");
 }
