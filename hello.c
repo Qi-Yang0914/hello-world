f7a9264ced4c6f33040c63723f9361bfa7560184
Melancholy_YQ <1500861686@qq.com>
2022-04-17 14:23:44 +0800

    add in dev branch
    Signed-off-by: Melancholy_YQ <1500861686@qq.com>

--- hello.c
+++ hello.c
@@ -4,9 +4,27 @@
 #include "stdio.h"
 #include "string.h"
 
+//  public function
+extern  int test(int x, int y);
+
+//  main
 int main(void)
 {
     printf("hello world!\n");
     printf("add dev branch\n");
     return  0;
+}
+/**
+ * @brief   compare
+ * 
+ * @param x 
+ * @param y 
+ * @return output the big one 
+ */
+int test(int x, int y)
+{
+    if(x > y)
+        return  x;
+    else
+        return  y;
 }
